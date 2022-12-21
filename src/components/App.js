60aad4bb3d971819d332ad57069bd2f61905d73e
shivam jain <113499149+shivamj2020@users.noreply.github.com>
2022-12-21 23:35:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,95 +1,225 @@
-import React from 'react'
-import '../styles/App.css';
-const App = () => {
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
 
-  let power=false;
 
-function display(number)
-{
-  if(power===true){
-    let present=document.getElementsByClassName('tv-container')[0].innerHTML;
-    console.log(number);
-    present+=`<center> <span style="margin-left:10px; display:inline-block;"></span> <center>`+number;
-    document.getElementsByClassName('tv-container')[0].innerHTML=present;
-    document.getElementById(number).classList.add('active-channel');
-    setTimeout(function() { document.getElementById(number).classList.remove('active-channel') } , 500);
-    
+#main {
+  text-align: center;
+}
 
-  }
+body {
+  margin: 0;
+  padding: 0;
+  font-family: roboto, serif;
+}
+.wrapper {
  
+  display: flex;
+  flex:0.5;
+  margin: 40 10%;
+  align-items: center;
   
 }
 
-function reset(){
-  power=!power;
-
-  if(power===false)
-  {
-    document.getElementsByClassName('tv-container')[0].innerHTML='<center>Power Off<center>';
-    document.getElementById('container').style.backgroundColor='black';
-  }
-  else{
-    document.getElementsByClassName('tv-container')[0].innerHTML='';
-    document.getElementById('container').style.backgroundColor='dark-grey';
-  }
+.tv-space {
   
+  position: relative;
+  padding: 10px;
+  height: auto;
+  width: 500px;
+  display: inline-block;
+}
+.tv-border {
+  float: left;
+  border-radius: 3px;
+  background: rgb(100,100,100) radial-gradient(circle at 50% 0, rgba(255,255,255,.65), rgba(255,255,255,.35));
+  box-shadow:
+  inset rgba(0,0,0,.6) 0 -1px 7px,
+  inset rgba(252,255,255,.3) 0px 1px 8px,
+  rgba(0,0,0,.8) 0 3px 8px 0px;
+  position: relative;
+  z-index: 100;
+}
+button {
+  cursor: pointer;
+}
+.tv-container {
+  margin: 7px;
+  margin-bottom: 0px;
+  background-color:white;
+  font-weight: 25px;
+  width: 480px;
+  height: 320px;
+  box-shadow: inset  0 5px 5px rgba(0,0,0,.9);
+  overflow: hidden;
 }
 
-function colorchange()
-{
-  document.getElementsByClassName('dot')[0].classList.add('clicked');
-    document.getElementsByClassName('dot')[1].classList.add('clicked');
-  
-    setTimeout(function() { document.getElementsByClassName('dot clicked')[0].classList.remove('clicked') } , 500);
-    setTimeout(function() { document.getElementsByClassName('dot clicked')[0].classList.remove('clicked') } , 500);
+
+.tv-container .screen-off {
+  background-color: #000;
+  height: 320px;
+  width: 480px;
+  opacity: 0;
 }
-  return (
+
+.pult {
+  display: inline-block;
+  background-color: #eee;
+  width: 213px;
+  margin: 10px auto;
+  font-family: 'console', 'PT Sans';
+  padding: 10px 10px 20px 10px;
+  border-radius: 10px;
+  
+  transform: translateX(-50%);
+  text-align: center;  
+  background: rgb(100,100,100) radial-gradient(circle at 50% 0, rgba(255,255,255,.65), rgba(255,255,255,.35));
+  box-shadow:
+  inset rgba(0,0,0,.6) 0 -3px 8px,
+  inset rgba(252,255,255,.7) 0 3px 8px,
+  rgba(0,0,0,.8) 0 3px 8px -3px;
+}
+.title {
+  position: relative;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 0px 0px;
+  width: 75px;
+  height: 30px;
+}
+
+
+
+.btn {
+  box-sizing: border-box;
+  display: inline-block;
+  border: 0;
+  text-align: center;
+  vertical-align: top;
+  width: 40px;
+  height: 40px;
+  line-height: 30px;
+  margin: 5px;
+  box-shadow: 0 0 10px 0px rgba(0,0,0,0.5);
+}
+.btn:active {
+  box-shadow: inset 0 0 10px 0px rgba(0,0,0,0.5);
+}
+.btn-big-inner:focus,
+.btn:focus {
+  outline: none;
+}
+.block {
+  display: inline-block;
+  vertical-align: top;
+}
+.block-channel {
+  width: 159px;
+}
+.block-navigate,
+block-round {
+  width: 213px;
+}
+.pult .btn.text {
+  font-size: 15px;
+  padding: 0;
+}
+.top-navigate,
+.channel,
+.round {
+  background-color: #1c1c1c;
+  color: #eee;
+  font-size: 35px;
+  font-weight: bold;
+  border-radius: 4px;
+}
+.active-channel {
+  color: #239B56;
+}
+.power-off {
+  background-color: #B03A2E;
+  position: relative;
+}
+.power-off:after {
+  content: '\f011';
+  font-family: FontAwesome;
+  position: absolute;
+  font-style: normal;
+  font-weight: normal;
+[2:23 pm, 21/08/2022] Deepshika: text-decoration: inherit;
+  position: absolute;
+  font-size: 20px;
+  top: 5px;
+  left: 11.5px;
+}
+
+.dot{
+  height: 10px;
+  width: 10px;
+  background-color: rgb(0, 0, 0);
+  border-radius: 50%;
+  display: inline-block;
+}
+
+.dot.clicked {
+  height: 10px;
+  width: 10px;
+  background-color: rgb(244, 9, 9);
+  border-radius: 50%;
+  display: inline-block;
+}
+#block{
+    width: 200px;
+    height: 300px;
+    border: 2px solid rgb(3, 1, 2);
+    border-radius: 15px;
+    padding: 20px;
+    background-color: black;
+}
+#container{
+width: 200px;
+height: 200px;
+border: 1px solid gray;
+margin: 10px;
+display: flex;
+align-items: center;
+justify-content: center;
+
+}
+.block .block-navigate{
+    width: 100%;
+    height: 50px;
     
-    <div id="main">
-        <div className="wrapper">
-        <div className="tv-space">
-          <div className="tv-border">
-            <div className="tv-container" id="container"style={{"color": "white",  "fontFamily": "roboto/serif",  "padding": "10px"}}>
-              <center>Power Off</center>
-            </div>
-            <div className="tv-nav" style={{"textAlign":"center"}}>
-              <br/> <span className="dot"></span> <br/><br/>
-            </div>
-          </div>
-          
-        </div>
-        
-        
-        <div className="pult">
-          <span className="dot"></span>
-          <div className="block title"></div>
-          <div className="block block-navigate">
-            
-            <button type="button" className="btn top-navigate power-off" id="power-off" onClick={reset} onMouseDown={colorchange}></button>
-           
-          </div>
-          <div className="block block-channel">
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(1)} onMouseDown={colorchange} id="1">1</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(2)} onMouseDown={colorchange} id="2">2</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(3)} onMouseDown={colorchange} id="3">3</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(4)} onMouseDown={colorchange} id="4">4</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(5)} onMouseDown={colorchange} id="5">5</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(6)} onMouseDown={colorchange} id="6">6</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(7)} onMouseDown={colorchange} id="7">7</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(8)} onMouseDown={colorchange} id="8">8</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(9)} onMouseDown={colorchange} id="9">9</button>
-            <button type="button" className="btn channel cnl-namber" onClick={()=> display(0)} onMouseDown={colorchange} id="0">0</button>
-          
-          </div>
-          
-         
-         </div>
-        </div>
+    
+}
+#power-on{
+    padding: 5px;
+    width: 40px;
+    height: 40px;
+    background-color: green;
+    float: left;
+    border-radius: 50px;
+}
+#power-off{
+    padding: 5px;
+    width: 40px;
+    height: 40px;
+    background-color: red;
+    float: right;
+    border-radius: 50px;
     
 
-    </div>
-  )
 }
 
+.block .block-channel{
+    padding: 10px;
+    width: 100%;
+    height: 200px;
+    margin-left: 6px;
+    margin-top: 10px;
+}
+.block .block-channel button{
+    padding: 20px;
+    margin: 2px;
+    border: 1px solid rgb(47, 204, 27);
+    border-radius: 50px;
 
-export default App;
+}
